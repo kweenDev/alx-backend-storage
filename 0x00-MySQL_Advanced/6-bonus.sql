1d63ec5e47a414eed789b6d77fe2923559c49fba
KweenDev <refiloe15radebe@gmail.com>
2024-10-17 07:52:00 +0200

    Create stored procedure AddBonus to add correction for user - SQL script to create AddBonus procedure that adds corrections for users - Procedure checks if project exists and creates it if necessary

--- 0x00-MySQL_Advanced/6-bonus.sql
+++ 0x00-MySQL_Advanced/6-bonus.sql
@@ -1,6 +1,6 @@
 -- Task 6: Create procedure to add a bonus for a student
 DELIMITER |
-CREATE PROCEDURE AddBonus(
+CREATE PROCEDURE AddBonus (
     IN user_id int,
     IN project_name varchar(255),
     IN score float
@@ -9,9 +9,7 @@ BEGIN
     INSERT INTO projects (name)
     SELECT project_name FROM DUAL
 
-    --- Check if project exists, if not, create it
     WHERE NOT EXISTS (SELECT * FROM projects WHERE name = project_name);
-    -- Add correction for the user
     INSERT INTO corrections (user_id, project_id, score)
     VALUES (user_id, (SELECT id FROM projects WHERE name = project_name), score);
 END;
