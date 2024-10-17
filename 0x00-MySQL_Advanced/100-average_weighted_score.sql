904aa0bfafdd4e6a8c79845c879d38c1298a0754
KweenDev <refiloe15radebe@gmail.com>
2024-10-17 07:03:19 +0200

    Create stored procedure for calculating average weighted score - SQL script to compute the weighted average score for a user based on project weights

---
+++ 0x00-MySQL_Advanced/100-average_weighted_score.sql
@@ -0,0 +1,17 @@
+-- Task 12: Create procedure to compute average weighted score for a user
+DELIMITER //
+CREATE PROCEDURE ComputeAverageScoreForUser(IN user_id INT)
+BEGIN
+    DECLARE weighted_avg_score FLOAT;
+
+    -- Calculate the weighted average score
+    SELECT SUM(score * weight) / SUM(weight) INTO weighted_avg_score
+    FROM corrections
+    JOIN projects ON corrections.project_id = projects.id
+    WHERE user_id = user_id;
+
+    -- Update user's average score
+    UPDATE users SET average_score = weighted_avg_score WHERE id = user_id;
+END;
+//
+DELIMITER ;
