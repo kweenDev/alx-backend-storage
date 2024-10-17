11831f2d34f2ffe30a9aeff6c87e4b8133a4bdcb
KweenDev <refiloe15radebe@gmail.com>
2024-10-17 07:38:21 +0200

    Create view to list students needing a meeting - SQL script to create a view that lists students with a score under 80 or missing a meeting

--- 0x00-MySQL_Advanced/11-need_meeting.sql
+++ 0x00-MySQL_Advanced/11-need_meeting.sql
@@ -1,4 +1,6 @@
 -- Task 11: Create a view that lists students needing a meeting
-CREATE VIEW need_meeting AS
-SELECT name FROM students
-WHERE score < 80 AND (last_meeting IS NULL OR last_meeting < DATE_SUB(CURDATE(), INTERVAL 1 MONTH));
+DROP VIEW IF EXISTS need_meeting;
+CREATE VIEW need_meeting
+AS SELECT name FROM students
+WHERE score < 80 AND (last_meeting IS NULL
+OR last_meeting < DATE_SUB(NOW(), INTERVAL 1 MONTH));
