acd92d713550d9810126e474d84ccc1d41170d74
KweenDev <refiloe15radebe@gmail.com>
2024-10-17 06:42:47 +0200

    List Glam rock bands by longevity - SQL script to display Glam rock bands and their lifespan - Use year formed and split to compute lifespan

--- 0x00-MySQL_Advanced/3-glam_rock.sql
+++ 0x00-MySQL_Advanced/3-glam_rock.sql
@@ -1,5 +1,5 @@
 -- Task 3: List all Glam rock bands ranked by longevity
-SELECT band_name, (2022 - formed) AS lifespan
-FROM metal_bands
-WHERE main_style = 'Glam rock'
-ORDER BY lifespan DESC;
+SELECT band_name, (IFNULL(split, '2022') - formed) AS lifespan
+    FROM metal_bands
+    WHERE main_style = '%Glam rock%'
+    ORDER BY lifespan DESC;
