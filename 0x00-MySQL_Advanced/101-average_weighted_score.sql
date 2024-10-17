1fbfa1fc2e6a0a6e52e1c9044fb1c319590e2f05
KweenDev <refiloe15radebe@gmail.com>
2024-10-17 07:04:49 +0200

    Create stored procedure to calculate weighted average score for all users - SQL script that calculates and stores the weighted average score for all students

---
+++ 0x00-MySQL_Advanced/101-average_weighted_score.sql
@@ -0,0 +1,24 @@
+-- Task 13: Create procedure to compute average weighted score for all users
+DELIMITER //
+CREATE PROCEDURE ComputeAverageScoreForUsers()
+BEGIN
+    DECLARE user_cursor CURSOR FOR SELECT id FROM users;
+    DECLARE done INT DEFAULT 0;
+
+    DECLARE CONTINUE HANDLER FOR NOT FOUND SET done = 1;
+
+    OPEN user_cursor;
+
+    read_loop: LOOP
+        FETCH user_cursor INTO @user_id;
+        IF done THEN
+            LEAVE read_loop;
+        END IF;
+
+        CAL ComputeAverageScoreForUser(@user_id);
+    END LOOP;
+
+    CLOSE user_cursor;
+END;
+//
+DELIMITER ;
