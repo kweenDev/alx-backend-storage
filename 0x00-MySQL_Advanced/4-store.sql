55a168eb6b024543db59c15dbd198bebb6dca982
KweenDev <refiloe15radebe@gmail.com>
2024-10-17 06:50:39 +0200

    Create trigger to decrease item quantity after order - SQL script to create a trigger that decreases item quantity after an order is placed - Ensures accurate inventory management through automatic updates

---
+++ 0x00-MySQL_Advanced/4-store.sql
@@ -0,0 +1,7 @@
+-- Task 4: Create trigger to decrease quantity after new order
+CREATE TRIGGER decrease_quantity AFTER INSERT ON orders
+FOR EACH ROW
+BEGIN
+    UPDATE items SET quantity = quantity - NEW.number
+    WHERE name = NEW.item_name;
+END;
