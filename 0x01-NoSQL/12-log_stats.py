c60e5ecf2850f97b0f8576e51b9f05083d0cfb3c
KweenDev <refiloe15radebe@gmail.com>
2024-12-03 13:52:19 +0200

    fix: Fix script with coherent code that provides some stats about Nginx logs stored in MongoDB

--- 0x01-NoSQL/12-log_stats.py
+++ 0x01-NoSQL/12-log_stats.py
@@ -8,31 +8,14 @@ import pymongo
 from pymongo import MongoClient
 
 
-def log_nginx_stats(mongo_collection):
-    """
-    Provides statistics about Nginx logs stored in MongoDB.
-
-    Returns:
-    None
-    """
-    print('{} logs'.format(mongo_collection.count_documents({})))
-    print('Methods:')
-    methods = ['GET', 'POST', 'PUT', 'PATCH', 'DELETE']
-    for method in methods:
-        req_count = len(list(mongo_collection.find({'method': method})))
-        print('\tmethod {}: {}'.format(method, req_count))
-    status_checks_count = len(list(
-        mongo_collection.find({'method': 'GET', 'path': '/status'})
-    ))
-    print('{} status check'.format(status_checks_count))
-
-
-def run():
-    '''Provides some stats about Nginx logs stored in MongoDB.
-    '''
-    client = MongoClient('mongodb://127.0.0.1:27017')
-    log_nginx_stats(client.logs.nginx)
-
-
-if __name__ == '__main__':
-    run()
+if __name__ == "__main__":
+    client = pymongo.MongoClient('mongodb://127.0.0.1:27017')
+    ng = client.logs.nginx
+    print(f"{ng.count_documents({})} logs")
+    print("Methods:")
+    for met in ["GET", "POST", "PUT", "PATCH", "DELETE"]:
+        print("\tmethod {}: {}".format(met, ng.count_documents({
+            "method": met
+        })))
+    print("{} status check".format(ng.count_documents(
+        {"$and": [{"method": "GET"}, {"path": "/status"}]})))
